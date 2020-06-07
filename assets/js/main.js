e4cd85605a99a1c872c5cabb7dd4e3ce7dd19ef2
Kinsella Kinsella <kinsellakaren@hotmail.com>
2020-06-07 19:28:02 +0000

    Committing changes to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,6 +46,63 @@ function to display results in index.html
 function displayResults(res) {
     for(var i = 0; i < res.items.length; i+=2) {
         item = res.items[i];
-        title;
+        title1 = item.VolmueInfo.title;
+        author1 = item.itemVolmueInfo;
+        publisher1 = item.volumeInfo.publisher;
+        bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
+        bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
+
     }
 }
+ item2 = response.items[i+1];
+        title2 = item2.volumeInfo.title;
+        author2 = item2.volumeInfo.authors;
+        publisher2 = item2.volumeInfo.publisher;
+        bookLink2 = item2.volumeInfo.previewLink;
+        bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
+        bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
+
+        // in production code, item.text should have the HTML entities escaped.
+        outputList.innerHTML += '<div class="row mt-4">' +
+                                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
+                                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                                '</div>';
+
+        console.log(outputList);
+      }
+   }
+
+   /*
+   * card element formatter using es6 backticks and templates (indivial card)
+   * @param bookImg title author publisher bookLink
+   * @return htmlCard
+   */
+   function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+     // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+     var viewUrl = 'book.html?isbn='+bookIsbn; //constructing link for bookviewer
+     var htmlCard = `<div class="col-lg-6">
+       <div class="card" style="">
+         <div class="row no-gutters">
+           <div class="col-md-4">
+             <img src="${bookImg}" class="card-img" alt="...">
+           </div>
+           <div class="col-md-8">
+             <div class="card-body">
+               <h5 class="card-title">${title}</h5>
+               <p class="card-text">Author: ${author}</p>
+               <p class="card-text">Publisher: ${publisher}</p>
+               <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>`
+     return htmlCard;
+   }
+
+   //handling error for empty search box
+   function displayError() {
+     alert("search term can not be empty!")
+   }
+
+});
