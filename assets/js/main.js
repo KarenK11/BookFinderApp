746a9f203c6c42cf44757c5f9cc85669d598642a
Kinsella Kinsella <kinsellakaren@hotmail.com>
2020-06-08 16:23:04 +0000

    Committing changes to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,59 @@
 $(document).ready(function() {
-    var item, title, author, publisher, bookLink, bookImg;
-    var outputList = document.getElementById("list-output");
-    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-    var apiKey = "key=AIzaSyCtIcvPpkhYk_KB7ityjQls-atqp1X6-Ks";
-    var placeHolder = '<img src="https://via.placeholder.com/150">';
-    var searchData; 
-})
+  var item, tile, author, publisher, bookLink, bookImg;
+  var outputList = document.getElementById("list-output");
+  var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+  var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+  var placeHldr = '<img src="https://via.placeholder.com/150">';
+  var searchData;
 
-//listener for Search button
-$("#search").click(function() {
-    outputList.innerHTML = "";
+  //listener for search button
+  $("#search").click(function() {
+    outputList.innerHTML = ""; //empty html output
     document.body.style.backgroundImage = "url('')";
-    searchData = $("#search-box").val();
-    //handling empty search input field
-    if(searchData === "" || searchData === null) {
-        displayError();
-    }
+     searchData = $("#search-box").val();
+     //handling empty search input field
+     if(searchData === "" || searchData === null) {
+       displayError();
+     }
     else {
-    // console.log(searchData);
-    // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData()});
-        $.ajax({
-            url: bookUrl + searchData,
-            dataType: "json",
-            success: function(response) {
-                console.log(response)
-                if(response.totalItems === 0) {
-                    alert("No results! Try again.")
-                }
-                else {
-                    $("title").anitem({"margin-top:" "5px"}, 1000);
-                    $(".book-list").css("visibility:", "visible");
-                    displayResults(response);
-                }
-            },
-            error: function() {
-                alert("Something went wrong!...<br>"+"Try again!");
+       // console.log(searchData);
+       // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData()});
+       $.ajax({
+          url: bookUrl + searchData,
+          dataType: "json",
+          success: function(response) {
+            console.log(response)
+            if (response.totalItems === 0) {
+              alert("no result!.. try again")
             }
+            else {
+              $("#title").animate({'margin-top': '5px'}, 1000); //search box animation
+              $(".book-list").css("visibility", "visible");
+              displayResults(response);
+            }
+          },
+          error: function () {
+            alert("Something went wrong.. <br>"+"Try again!");
+          }
         });
-    }
-    $("#search-box").val("");
-});
+      }
+      $("#search-box").val(""); //clearn search box
+   });
 
-/*
-function to display results in index.html
-@param res
-*/
-function displayResults(response) {
-    for(var i = 0; i < response.items.length; i+=2) {
+   /*
+   * function to display result in index.html
+   * @param response
+   */
+   function displayResults(response) {
+      for (var i = 0; i < response.items.length; i+=2) {
         item = response.items[i];
-        title1 = item.VolmueInfo.title;
-        author1 = item.VolmueInfo.authors;
+        title1 = item.volumeInfo.title;
+        author1 = item.volumeInfo.authors;
         publisher1 = item.volumeInfo.publisher;
         bookLink1 = item.volumeInfo.previewLink;
         bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
         bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
 
-    }
-}
         item2 = response.items[i+1];
         title2 = item2.volumeInfo.title;
         author2 = item2.volumeInfo.authors;
@@ -74,7 +71,6 @@ function displayResults(response) {
         console.log(outputList);
       }
    }
-
    /*
    * card element formatter using es6 backticks and templates (indivial card)
    * @param bookImg title author publisher bookLink
