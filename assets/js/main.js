b2a53505de97cda1425188ff39f0b9d5ef5b60eb
Kinsella Kinsella <kinsellakaren@hotmail.com>
2020-07-08 10:27:10 +0000

    Committing changes which include changing how my function is called on line 10, from submit to click

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   var searchData;
 
   //listener for search button
-  $("#search").submit(function() {
+  $("#search").click(function() {
     outputList.innerHTML = ""; //empty html output
     document.body.style.backgroundImage = "url('')";
      searchData = $("#search-box").val();
@@ -48,15 +48,16 @@ $(document).ready(function() {
    * @param response
    */
    function displayResults(response) {
-      for (var i = 0; i < response.items.length; i+=2) {
+      for (var i = 0; i < response.items.length; i+=1) {
         item = response.items[i];
         title1 = item.volumeInfo.title;
         author1 = item.volumeInfo.authors;
         publisher1 = item.volumeInfo.publisher;
         bookLink1 = item.volumeInfo.previewLink;
+        console.log(item.volumeInfo)
         bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
         bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
-
+/*
         item2 = response.items[i+1];
         title2 = item2.volumeInfo.title;
         author2 = item2.volumeInfo.authors;
@@ -64,11 +65,11 @@ $(document).ready(function() {
         bookLink2 = item2.volumeInfo.previewLink;
         bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
         bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
-
+*/
         // in production code, item.text should have the HTML entities escaped.
         outputList.innerHTML += '<div class="row mt-4">' +
                                 formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
-                                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                                /*formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +*/
                                 '</div>';
 
         console.log(outputList);
